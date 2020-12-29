248d8d327624bfbbaa8a3ac3d767e71ac584ca32
Scott MacDonnell <scottsmacdonnell@gmail.com>
2020-12-28 21:08:56 -0500

    Fix twitter widget load issue

--- components/Twitter/Timeline/index.jsx
+++ components/Twitter/Timeline/index.jsx
@@ -1,6 +1,7 @@
 export default function Timeline() {
   return (
     <div className="Timeline">
+      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8" />
       <a class="twitter-timeline" 
         data-theme="dark" 
         href="https://twitter.com/deepsixca?ref_src=twsrc%5Etfw"
@@ -8,7 +9,6 @@ export default function Timeline() {
         data-height="500"
         data-chrome="nofooter noborders noscrollbar"
       >Tweets by deepsixca</a>
-      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     </div>
   )
 }
