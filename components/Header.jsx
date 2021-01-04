77d755ef53f4ce4e317e68f4b7c9acceac838ce1
Scott MacDonnell <scottsmacdonnell@gmail.com>
2021-01-04 16:15:31 -0500

    Add skeleton for Header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const fetcher = async (url) => {
 export default function Header({ name }) {
   const { data, error } = useSWR(() => `/api/spotify/get-artist`, fetcher)
   if (error) return <div>{error.message}</div>
-  if (!data) return <p>Header</p>
+  if (!data) return <HeaderSkeleton />
   const { artist } = data
   return (
     <div className={styles.Header}>
@@ -29,7 +29,22 @@ export default function Header({ name }) {
             <h1>{name}</h1>
           </div>
         </div>
+      </div>
+    </div>
+  )
+}
 
+function HeaderSkeleton() {
+  return (
+    <div className={styles.HeaderSkeleton}>
+      <Navbar />
+      
+      <div className="Content">
+        <div className={styles.HeaderSkeletonContent}>
+          <div className={styles.HeaderSkeletonContentMeta}>
+            <div className={styles.HeaderSkeletonContentMetaName} />
+          </div>
+        </div>
       </div>
     </div>
   )
