b0366a709ad066857fbdb3349fd3a6c98c36b916
Scott MacDonnell <scottsmacdonnell@gmail.com>
2021-01-03 19:25:09 -0500

    Remove LatestRelease component from music page

--- pages/music.jsx
+++ pages/music.jsx
@@ -2,7 +2,6 @@ import Head from 'next/head'
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import LatestRelease from '../components/LatestRelease'
 import Releases from '../components/Releases'
 
 export default function Checkout() {
@@ -21,7 +20,6 @@ export default function Checkout() {
         </header>
 
         <main>
-          <LatestRelease />
           <Releases />
         </main>
 
