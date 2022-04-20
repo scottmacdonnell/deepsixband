67f5b7e39d6e6321b879155129157bf94be67933
Scott MacDonnell <scottsmacdonnell@gmail.com>
2022-04-20 12:58:42 -0400

    Upgrade dependencies and fix release versions

--- pages/api/spotify/get-releases.js
+++ pages/api/spotify/get-releases.js
@@ -9,10 +9,12 @@ export default async (_, res) => {
 
   const releases = await response.json()
 
+  const formattedReleases = releases.items.filter((v,i,a)=>a.findIndex(v2=>(v2.name===v.name))===i)
+
   res.setHeader(
     'Cache-Control',
     'public, s-maxage=60, stale-while-revalidate=30'
   )
 
-  return res.status(200).json({ releases: releases.items })
+  return res.status(200).json({ releases: formattedReleases })
 }
