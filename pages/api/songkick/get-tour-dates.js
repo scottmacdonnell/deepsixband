242b2d2c38a3a597ab9adfce86cd8d749a4dd55c
Scott MacDonnell <scottsmacdonnell@gmail.com>
2022-06-19 14:58:35 -0400

    Minor changes

--- pages/api/songkick/get-tour-dates.js
+++ pages/api/songkick/get-tour-dates.js
@@ -1,7 +1,6 @@
 export default async (_, res) => {
   const artistID = '10109726'
-  // const artistID = '8723059'
-  const requestURL = `https://api.songkick.com/api/3.0/artists/${artistID}/calendar.json?apikey=${process.env.SONGKICK_KEY}&per_page=5`
+  const requestURL = `https://api.songkick.com/api/3.0/artists/${artistID}/calendar.json?apikey=bes5VlOjJoyrXx6O&per_page=5`
 
   const response = await fetch(requestURL)
   const tourData = await response.json()
